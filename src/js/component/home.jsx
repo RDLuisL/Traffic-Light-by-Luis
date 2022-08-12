dddcdf464170e6229616ac967c37dcbad5c688d9
Luis Luna <luislunandroid@gmail.com>
2022-08-12 19:47:15 -0400

    version2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
+	//const [toggle, setToggle] = useState(false);
+	const [redPower, setRedPower] = useState(false);
+	const [yellowPower, setYellowPower] = useState(false);
+	const [greenPower, setGreenPower] = useState(false);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5 d-flex justify-content-center flex-column align-items-center">
+			<div
+				style={{
+					backgroundColor: "black",
+					width: "1rem",
+					height: "12rem"
+				}}></div>
+			<div
+				style={{
+					backgroundColor: "black",
+					width: "8rem",
+					height: "16rem"
+				}}>
+				<div className="lights d-flex justify-content-center flex-column align-items-center pt-4">
+					<div
+						onClick={() => {
+							setRedPower(!redPower);
+							setYellowPower(false);
+							setGreenPower(false);
+						}}
+						className={`red-on-${redPower}`}
+						style={{
+							backgroundColor: "red",
+							width: "4rem",
+							height: "4rem",
+							borderRadius: "50%"
+						}}></div>
+					<div
+						onClick={() => {
+							setRedPower(false);
+							setYellowPower(!yellowPower);
+							setGreenPower(false);
+						}}
+						className={`yellow-on-${yellowPower}`}
+						style={{
+							backgroundColor: "yellow",
+							width: "4rem",
+							height: "4rem",
+							borderRadius: "50%"
+						}}></div>
+					<div
+						onClick={() => {
+							setRedPower(false);
+							setYellowPower(false);
+							setGreenPower(!greenPower);
+						}}
+						className={`green-on-${greenPower}`}
+						style={{
+							backgroundColor: "green",
+							width: "4rem",
+							height: "4rem",
+							borderRadius: "50%"
+						}}></div>
+				</div>
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+
--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
+//include bootstrap npm library into the bundle
+import "bootstrap";
+
+//include your index.scss file into the bundle
+import "../styles/index.scss";
 
 //import your own components
 import Home from "./component/home.jsx";
