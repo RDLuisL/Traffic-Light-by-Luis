4956cbd25555940a258b743e569ea41f517272a2
LuisL <luislunandroid@gmail.com>
2022-08-13 01:42:36 +0000

    version 3.0

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
-import "bootstrap";
+
 
 //include your index.scss file into the bundle
-import "../styles/index.scss";
+import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
