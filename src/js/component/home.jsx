cc3bec7ccbad8b8123370152835d1098e46d0ab6
LuisL <luislunandroid@gmail.com>
2022-11-27 15:31:06 +0000

    version con el semaforo y el fondo, sólo falta solapar las luces al semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import semaforo from "../../img/semaforo.png"
+import semaforo from "../../img/Semaforo.png"
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -12,56 +12,47 @@ function Home() {
 	const [greenPower, setGreenPower] = useState(false);
 
 	return (
-		<div className="text-center mt-5 d-flex justify-content-center flex-column align-items-center">
-
-
-			<div style={
-				{backgroundImage: `url(${semaforo})`}
-			}
-			
-			className="lights d-flex justify-content-center flex-column align-items-center pt-4">
-				<div
-					onClick={() => {
-						setRedPower(!redPower);
-						setYellowPower(false);
-						setGreenPower(false);
-					} }
-					className={`red-on-${redPower}`}
-					style={{
-						backgroundColor: "red",
-						width: "4rem",
-						height: "4rem",
-						borderRadius: "50%"
-					}}></div>
-				<div
-					onClick={() => {
-						setRedPower(false);
-						setYellowPower(!yellowPower);
-						setGreenPower(false);
-					} }
-					className={`yellow-on-${yellowPower}`}
-					style={{
-						backgroundColor: "yellow",
-						width: "4rem",
-						height: "4rem",
-						borderRadius: "50%"
-					}}></div>
-				<div
-					onClick={() => {
-						setRedPower(false);
-						setYellowPower(false);
-						setGreenPower(!greenPower);
-					} }
-					className={`green-on-${greenPower}`}
-					style={{
-						backgroundColor: "green",
-						width: "4rem",
-						height: "4rem",
-						borderRadius: "50%"
-					}}></div>
-			</div>
-
-		</div>
+		<img src="https://imagizer.imageshack.com/img923/6300/lPMSQi.png" className="semaforo background-image">
+					<div className="overlay lights d-flex justify-content-center flex-column align-items-center pt-4">
+							<div onClick={() => {
+								setRedPower(!redPower); 
+								setYellowPower(false); 
+								setGreenPower(false);}}
+								className={`red-on-${redPower}`}
+								style={{
+									backgroundColor: "red",
+									width: "4rem",
+									height: "4rem",
+									borderRadius: "50%"}}>
+							</div>
+							<div
+								onClick={() => {
+								setRedPower(false);
+								setYellowPower(!yellowPower);
+								setGreenPower(false);
+								} }
+								className={`yellow-on-${yellowPower}`}
+								style={{
+									backgroundColor: "yellow",
+									width: "4rem",
+									height: "4rem",
+									borderRadius: "50%"
+								}}></div>
+							<div
+								onClick={() => {
+								setRedPower(false);
+								setYellowPower(false);
+								setGreenPower(!greenPower);
+								} }
+								className={`green-on-${greenPower}`}
+								style={{
+									backgroundColor: "green",
+									width: "4rem",
+									height: "4rem",
+									borderRadius: "50%"}}>
+							</div>
+					</div>
+		</img>
 	);
 }
 
